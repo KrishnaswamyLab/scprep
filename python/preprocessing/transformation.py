3414f228bd4c09dbd187e5ac630247e08e94baa2
Scott Gigante <scottgigante@gmail.com>
2018-06-15 16:42:23 -0400

    add sqrt, log, arcsinh transformation

--- python/preprocessing/transformation.py
+++ python/preprocessing/transformation.py
@@ -3,16 +3,40 @@
 
 import numpy as np
 from scipy import sparse
+import warnings
 
 
-def sqrt_transform():
-    pass
+def _transform(X, fun):
+    if sparse.issparse(X):
+        if isinstance(X, (sparse.lil_matrix, sparse.dok_matrix)):
+            X = X.tocsr()
+        X.data = fun(X.data)
+    else:
+        X = fun(X)
+    return X
 
 
-def log_transform(data, pseudocount=1):
-    pass
+def sqrt_transform(X):
+    if any(X < 0):
+        raise ValueError("Cannot square root transform negative values")
+    return _transform(X, np.sqrt)
 
 
-def arcsinh_transform(data, cofactor=5):
-    if cofactor > 0:
-        data = np.arcsinh(data / cofactor)
+def log_transform(X, pseudocount=1):
+    if any(X < 0):
+        raise ValueError("Cannot log transform negative values")
+    if pseudocount != 1 and (sparse.issparse(X) or
+                             isinstance(X, pd.SparseDataFrame)):
+        warnings.warn("log transform on sparse data requires pseudocount=1",
+                      RuntimeWarning)
+        pseudocount = 1
+    return _transform(X, lambda X: np.log(X + 1))
+
+
+def arcsinh_transform(X, cofactor=5):
+    if cofactor <= 0:
+        raise ValueError("Expected cofactor > 0 or None. "
+                         "Got {}".format(cofactor))
+    if cofactor is not None:
+        X = X / cofactor
+    return _transform(X, np.arcsinh)
---
+++ python/test/test_transformation.py
@@ -0,0 +1,70 @@
+import numpy as np
+from scipy import sparse
+import pandas as pd
+from functools import partial
+from preprocessing import transformations
+
+
+def to_array(X):
+    if sparse.issparse(X):
+        X = X.toarray()
+    elif isinstance(X, pd.SparseDataFrame):
+        X = X.to_dense().values
+    elif isinstance(X, pd.DataFrame):
+        X = X.values
+    return X
+
+
+def all_equal(X, Y):
+    X = to_array(X)
+    Y = to_array(Y)
+    return np.all(X == Y)
+
+
+def all_close(X, Y):
+    X = to_array(X)
+    Y = to_array(Y)
+    return np.allclose(X, Y)
+
+
+def any(condition):
+    """np.any doesn't handle data frames
+    """
+    return np.sum(np.any(condition)) > 0
+
+
+def test_transform(transform, lambda_transform, seed=42, check=all_equal):
+    np.random.seed(seed)
+    X = np.random.normal(0, 1, [1000, 1000]) * \
+        np.random.poisson(0.1, [1000, 1000])
+    matrix_funs = [
+        np.array,
+        sparse.csr_matrix,
+        sparse.csc_matrix,
+        sparse.bsr_matrix,
+        sparse.csc_matrix,
+        sparse.lil_matrix,
+        sparse.dok_matrix,
+        sparse.dia_matrix,
+        pd.DataFrame,
+        partial(pd.SparseDataFrame, default_fill_value=0.0)]
+    X = np.abs(X)
+    transform_X = lambda_transform(X)
+    for fun in matrix_funs:
+        Y = transform(fun(X.copy()))
+        assert check(Y, transform_X), "{} failed on {}".format(
+            transform.__name__,
+            fun.__name__)
+        if sparse.issparse(X):
+            assert sparse.issparse(Y)
+        if isinstance(X, pd.DataFrame):
+            assert X.columns == Y.columns
+            assert X.index == Y.index
+        if isinstance(X, pd.SparseDataFrame):
+            assert X.density == Y.density
+
+
+test_transform(transformations.sqrt_transform, lambda X: np.sqrt(X))
+test_transform(transformations.log_transform, lambda X: np.log(X + 1))
+test_transform(transformations.arcsinh_transform,
+               lambda X: np.arcsinh(X / 5), check=all_close)
