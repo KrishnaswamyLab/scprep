e7b664d54d55f970a55b4f09f752c6c3c9532b80
Scott Gigante <scottgigante@gmail.com>
2018-06-18 09:36:33 -0400

    make sanitization tests pass

--- python/preprocessing/sanitization.py
+++ python/preprocessing/sanitization.py
@@ -2,27 +2,30 @@
 # (C) 2018 Krishnaswamy Lab GPLv2
 
 import pandas as pd
-import matplitlib.pyplot as plt
 import numpy as np
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    pass
 
 
 def remove_empty_genes(data):
     gene_sums = data.sum(axis=0)
     to_keep = gene_sums > 0
     if isinstance(data, pd.DataFrame):
-        data = data.ix[:, to_keep]
+        data = data[data.columns[to_keep].tolist()]
     else:
         data = data[:, to_keep]
     return data
 
 
 def remove_empty_cells(data):
-    gene_sums = data.sum(axis=0)
-    to_keep = gene_sums > 0
+    cell_sums = library_size(data)
+    to_keep = cell_sums > 0
     if isinstance(data, pd.DataFrame):
-        data = data.ix[:, to_keep]
+        data = data.loc[to_keep]
     else:
-        data = data[:, to_keep]
+        data = data[to_keep, :]
     return data
 
 
@@ -31,7 +34,7 @@ def library_size(data):
         cell_sums = data.sum(axis=1)
     except MemoryError:
         # pandas sparse dataframes do weird stuff
-        if isinstance(data, pd.SparseDataFrame):
+        if False:  # isinstance(data, pd.SparseDataFrame):
             split = np.arange(0, data.shape[0], data.shape[0] // 10)
             split = np.concatenate([split, [data.shape[0]]])
             cell_sums = []
@@ -45,6 +48,11 @@ def library_size(data):
 
 
 def plot_library_size(data, bins=30, cutoff=None):
+    try:
+        plt
+    except NameError:
+        print("matplotlib not found. "
+              "Please install it with e.g. `pip install --user matplotlib`")
     cell_sums = library_size(data)
     plt.hist(cell_sums, bins=bins)
     if cutoff is not None:
--- python/test/test_sanitization.py
+++ python/test/test_sanitization.py
@@ -1,4 +1,4 @@
-from preprocessing import io
+from preprocessing import sanitization, io
 from sklearn.utils.testing import assert_warns_message
 import pandas as pd
 import numpy as np
@@ -14,3 +14,38 @@ def load_10X(**kwargs):
     return io.load_10X(os.path.join(data_dir, "test_10X"), **kwargs)
 
 # TODO: write tests
+
+
+def test_remove_empty_cells():
+    data = load_10X(sparse=False)
+    sanitized_data = sanitization.remove_empty_cells(data)
+    assert sanitized_data.shape[1] == data.shape[1]
+    assert not np.any(sanitized_data.sum(1) == 0)
+
+
+def test_remove_empty_cells_sparse():
+    data = load_10X(sparse=True)
+    sanitized_data = sanitization.remove_empty_cells(data)
+    assert sanitized_data.shape[1] == data.shape[1]
+    assert not np.any(sanitized_data.sum(1) == 0)
+
+
+def test_remove_empty_genes():
+    data = load_10X(sparse=False)
+    sanitized_data = sanitization.remove_empty_genes(data)
+    assert sanitized_data.shape[0] == data.shape[0]
+    assert not np.any(sanitized_data.sum(0) == 0)
+
+
+def test_remove_empty_genes_sparse():
+    data = load_10X(sparse=True)
+    sanitized_data = sanitization.remove_empty_genes(data)
+    assert sanitized_data.shape[0] == data.shape[0]
+    assert not np.any(sanitized_data.sum(0) == 0)
+
+
+def test_library_size_filter():
+    data = load_10X(sparse=True)
+    sanitized_data = sanitization.filter_library_size(data, 100)
+    assert sanitized_data.shape[1] == data.shape[1]
+    assert not np.any(sanitized_data.sum(1) < 100)
