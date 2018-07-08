d0797601b2948032795372f04edc062afca4d3b0
Scott Gigante <scottgigante@gmail.com>
2018-07-08 11:15:22 -0400

    add get gene set function

--- python/scprep/utils.py
+++ python/scprep/utils.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numbers
 from scipy import sparse
 import warnings
+import re
 
 
 def matrix_any(condition):
@@ -93,3 +94,44 @@ def select_rows(data, idx):
     if data.shape[0] == 0:
         warnings.warn("Selecting 0 rows.", UserWarning)
     return data
+
+
+def get_gene_set(data, starts_with=None, ends_with=None, regex=None):
+    """Get a list of genes from data
+
+    Parameters
+    ----------
+    data : array-like, shape=[n_samples, n_features] or [n_features]
+        Input pd.DataFrame, or list of gene names
+    starts_with : str or None, optional (default: None)
+        If not None, only return gene names that start with this prefix
+    ends_with : str or None, optional (default: None)
+        If not None, only return gene names that end with this suffix
+    regex : str or None, optional (default: None)
+        If not None, only return gene names that match this regular expression
+
+    Returns
+    -------
+    genes : list-like, shape<=[n_features]
+        List of matching genes
+    """
+    if len(data.shape) > 1:
+        try:
+            data = data.columns
+        except AttributeError:
+            raise TypeError("data must be a list of gene name or a pandas "
+                            "DataFrame. Got {}".format(type(data).__name__))
+    mask = np.full_like(data, True, dtype=bool)
+    if starts_with is not None:
+        start_match = np.vectorize(lambda x: x.startswith(starts_with))
+        mask = np.logical_and(mask, start_match(data))
+    if ends_with is not None:
+        end_match = np.vectorize(lambda x: x.endswith(ends_with))
+        mask = np.logical_and(mask, end_match(data))
+    if regex is not None:
+        regex = re.compile(regex)
+        regex_match = np.vectorize(lambda x: bool(regex.match(x)))
+        mask = np.logical_and(mask, regex_match(data))
+    return data[mask]
+
+get_gene_set(X, starts_with="D")
