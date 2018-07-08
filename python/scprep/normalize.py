64b79960ac1b6dd57d496b7e91430f45821d9e71
Scott Gigante <scottgigante@gmail.com>
2018-07-08 11:14:34 -0400

    add rescale options to libsize norm

--- python/scprep/normalize.py
+++ python/scprep/normalize.py
@@ -6,10 +6,11 @@ from sklearn.preprocessing import normalize
 import numpy as np
 from scipy import sparse
 import pandas as pd
+import numbers
 from .filter import library_size
 
 
-def library_size_normalize(data):
+def library_size_normalize(data, rescale='median'):
     """Performs L1 normalization on input data
     Performs L1 normalization on input data such that the sum of expression
     values for each cell sums to 1
@@ -18,15 +19,18 @@ def library_size_normalize(data):
 
     Parameters
     ----------
-    data : ndarray [n,p]
-        2 dimensional input data array with n cells and p dimensions
-    rescale : {'mean', 'median'}, int or `None`, optional (default: 'median')
-        Rescaling strategy. TODO.
+    data : array-like, shape=[n_samples, n_features]
+        Input data
+    rescale : {'mean', 'median'}, float or `None`, optional (default: 'median')
+        Rescaling strategy. If 'mean' or 'median', normalized cells are scaled
+        back up to the mean or median expression value. If a float,
+        normalized cells are scaled up to the given value. If `None`, no
+        rescaling is done and all cells will have normalized library size of 1.
 
     Returns
     -------
-    data_norm : ndarray [n, p]
-        2 dimensional array with normalized gene expression values
+    data_norm : array-like, shape=[n_samples, n_features]
+        Library size normalized output data
     """
     # pandas support
     columns, index = None, None
@@ -37,7 +41,18 @@ def library_size_normalize(data):
     elif isinstance(data, pd.DataFrame):
         columns, index = data.columns, data.index
 
-    median_transcript_count = np.median(np.array(library_size(data)))
+    if rescale == 'median':
+        rescale = np.median(np.array(library_size(data)))
+    elif rescale == 'mean':
+        rescale = np.mean(np.array(library_size(data)))
+    elif isinstance(rescale, numbers.Number):
+        pass
+    elif rescale is None:
+        rescale = 1
+    else:
+        raise ValueError("Expected rescale in ['median', 'mean'], a number "
+                         "or `None`. Got {}".format(rescale))
+
     if sparse.issparse(data) and data.nnz >= 2**31:
         # check we can access elements by index
         try:
@@ -68,7 +83,7 @@ def library_size_normalize(data):
     # norm = 'l1' computes the L1 norm which computes the
     # axis = 1 independently normalizes each sample
 
-    data_norm = data_norm * median_transcript_count
+    data_norm = data_norm * rescale
     if columns is not None:
         # pandas dataframe
         if sparse.issparse(data_norm):
