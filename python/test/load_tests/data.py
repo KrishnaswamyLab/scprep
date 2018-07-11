2a9a40d2f64eeb4ee7086623fd4dab25c584d1ec
Scott Gigante <scottgigante@gmail.com>
2018-07-10 21:42:50 -0400

    fix data_dir

--- python/test/load_tests/data.py
+++ python/test/load_tests/data.py
@@ -4,7 +4,11 @@ import os
 data_dir = os.getcwd().split(os.path.sep)
 while data_dir[-1] in ["load_tests", "test", "python"]:
     data_dir = data_dir[:-1]
+
 data_dir = data_dir + ["data", "test_data"]
+end = data_dir[1:] if len(data_dir) > 2 else [data_dir[1]]
+data_dir = [data_dir[0]] + [os.path.sep] + end
+data_dir = os.path.join(*data_dir)
 
 
 def load_10X(**kwargs):
