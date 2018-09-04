b74e4a014cc3356e840fa397a1d30907d7d0d969
Scott Gigante <scottgigante@gmail.com>
2018-09-03 22:21:53 -0400

    attempted fix to test_reduce

--- python/test/test_reduce.py
+++ python/test/test_reduce.py
@@ -5,15 +5,15 @@ from functools import partial
 
 
 def test_pca():
-    X = data.generate_positive_sparse_matrix(shape=[50, 1000])
+    X = data.generate_positive_sparse_matrix(shape=[100, 1000])
     Y = decomposition.PCA(100, random_state=42).fit_transform(X)
     matrix.check_dense_matrix_types(
         X, utils.check_output_equivalent,
         Y=Y, transform=scprep.reduce.pca,
         n_pca=100, seed=42)
-    Y = decomposition.PCA(49, svd_solver='full').fit_transform(X)
+    Y = decomposition.PCA(50, svd_solver='full').fit_transform(X)
     matrix.check_sparse_matrix_types(
         X, utils.check_output_equivalent,
         Y=Y, transform=scprep.reduce.pca,
         check=partial(utils.assert_all_close, rtol=1e-3, atol=1e-5),
-        n_pca=49, svd_multiples=8, seed=42)
+        n_pca=50, svd_multiples=8, seed=42)
