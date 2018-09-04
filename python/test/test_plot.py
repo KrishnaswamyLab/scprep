9148b3ee8f39a4f79e9ce2d2026c474b5d10e4ef
Scott Gigante <scottgigante@gmail.com>
2018-09-03 22:28:09 -0400

    python agnostic test

--- python/test/test_plot.py
+++ python/test/test_plot.py
@@ -13,6 +13,6 @@ def test_plot_histogram():
         percentile=90, log='y', ax=ax)
     assert_raise_message(
         TypeError,
-        "Expected ax as a matplotlib.axes.Axes. Got <class 'str'>",
+        "Expected ax as a matplotlib.axes.Axes. Got ",
         scprep.plot.plot_library_size,
         X, ax="invalid")
