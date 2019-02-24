272bb429ec7fd1f68ec39e5d589a0a0e8a96cc10
Scott Gigante <scottgigante@gmail.com>
2019-02-24 16:17:44 -0500

    Revert "use <br> instead"
    This reverts commit b02e353619ba5b6dba3dd0112bddcad27f9764f8.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -193,9 +193,8 @@ nbsphinx_prolog = r"""
 
     .. nbinfo::
 
-        This page was generated from `{{ docname }}`__ by nbsphinx_.
-          :raw-html:`<br />`
-        Interactive version:
+        | This page was generated from `{{ docname }}`__ by nbsphinx_.
+        | Interactive version:
           :raw-html:`<a href="https://mybinder.org/v2/gh/KrishnaswamyLab/scprep/{{ env.config.branch }}?filepath={{ docname }}"><img alt="Run in Binder" src="https://mybinder.org/badge.svg" style="vertical-align:text-bottom"></a>`
           :raw-html:`<a href="https://colab.research.google.com/github/KrishnaswamyLab/scprep/blob/{{ env.config.branch }}/{{ docname }}"><img alt="Run in Colab" src="https://colab.research.google.com/assets/colab-badge.svg" style="vertical-align:text-bottom"></a>`
 
