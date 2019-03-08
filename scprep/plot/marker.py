51fa4bd7f09fe621403199419d5012c043313b10
Scott Gigante <scottgigante@gmail.com>
2019-03-08 15:36:26 -0500

    sorted marker plot

--- scprep/plot/marker.py
+++ scprep/plot/marker.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy.cluster import hierarchy
 
 from .. import utils, stats, select
 from .utils import (_get_figure, show,
@@ -9,12 +10,20 @@ from .tools import label_axis
 
 @utils._with_pkg("matplotlib")
 def marker_plot(data, clusters, markers, gene_names=None,
-                normalize_expression=True, cmap='magma',
+                normalize_expression=True, normalize_emd=True,
+                reorder_tissues=True,
+                reorder_markers=True,
+                cmap='magma',
                 title=None, figsize=None,
                 ax=None, fontsize=None):
-    """
+    """Marker gene enrichmewnt plot
+
     Generate a plot indicating the expression level and enrichment of
     a set of marker genes for each cluster.
+
+    Color of each point indicates the expression of each gene in each cluster.
+    The size of each point indicates how differentially expressed each gene is
+    in each cluster.
 
     Parameters
     ----------
@@ -29,8 +38,10 @@ def marker_plot(data, clusters, markers, gene_names=None,
         If a list, a list of marker genes.
     gene_names : list-like, shape=[n_genes]
         List of gene names.
-    normalize_expression : bool, optional (default: True)
-        Normalize the expression of each row.
+    normalize_{expression,emd} : bool, optional (default: True)
+        Normalize the expression and EMD of each row.
+    reorder_{tissues,markers} : bool, optional (default: True)
+        Reorder tissues and markers according to hierarchical clustering=
     cmap : str or matplotlib colormap, optional (default: 'inferno')
         Colormap with which to color points.
     title : str or None, optional (default: None)
@@ -104,6 +115,7 @@ def marker_plot(data, clusters, markers, gene_names=None,
             out_cluster_expr = data[~in_cluster]
             data_clust[cluster] = (in_cluster_expr, out_cluster_expr)
 
+        # build points coordinate, color and size arrays
         for j, marker in enumerate(markers):
             s_row = []
             c_row = []
@@ -122,15 +134,79 @@ def marker_plot(data, clusters, markers, gene_names=None,
                 c_row.append(np.mean(marker_expr))
             # row normalize
             s_row = np.array(s_row)
-            s_row = 150 * s_row / np.max(s_row)
+            if normalize_emd and np.max(s_row) != 0:
+                s_row = 150 * s_row / np.max(s_row)
             c_row = np.array(c_row)
-            if normalize_expression:
+            if normalize_expression and np.max(c_row) != 0:
                 c_row = c_row / np.max(c_row)
             s.append(s_row)
             c.append(c_row)
 
         s = np.concatenate(s)
+        if not normalize_emd:
+            s = 150 * s / np.max(s)
         c = np.concatenate(c)
+
+        # reorder y axis
+        if tissues is not None:
+            tissue_names = np.unique(tissues)
+            if reorder_tissues:
+                # cluster tissues hierarchically using mean size and color
+                tissue_features = []
+                for tissue in tissue_names:
+                    tissue_data = []
+                    for cluster in cluster_names:
+                        tissue_cluster_idx = np.where(
+                            (np.array(tissue_labels) == tissue) & (
+                                np.array(cluster_labels) == cluster))
+                        tissue_data.append(np.vstack(
+                            [s[tissue_cluster_idx],
+                             c[tissue_cluster_idx]]).mean(axis=1))
+                    tissue_features.append(np.concatenate(tissue_data))
+                tissue_features = np.array(tissue_features)
+                # normalize
+                tissue_features = tissue_features / \
+                    np.sqrt(np.sum(tissue_features ** 2))
+                tissues_order = hierarchy.leaves_list(
+                    hierarchy.linkage(tissue_features))
+            else:
+                # keep tissues in order
+                tissues_order = np.arange(len(tissue_names))
+            marker_groups_order = [
+                np.arange(len(markers))[tissues == tissue_names[i]]
+                for i in tissues_order]
+        else:
+            # only one tissue
+            marker_groups_order = [np.arange(len(markers))]
+
+        if reorder_markers:
+            # cluster markers hierarchically using mean size and color
+            markers_order = []
+            for marker_group in marker_groups_order:
+                marker_names = markers[marker_group]
+                marker_features = []
+                for marker in marker_names:
+                    marker_idx = np.array(marker_labels) == marker
+                    marker_features.append(np.concatenate(
+                        [s[marker_idx], c[marker_idx]]))
+                marker_features = np.array(marker_features)
+                # normalize
+                marker_features = marker_features / \
+                    np.sqrt(np.sum(marker_features ** 2))
+                marker_group_order = hierarchy.leaves_list(
+                    hierarchy.linkage(marker_features))
+                markers_order.append(marker_group[marker_group_order])
+            markers_order = np.concatenate(markers_order)
+        else:
+            # keep markers in order
+            markers_order = np.concatenate(marker_groups_order)
+
+        # reposition y coordinates
+        y = np.array(y)
+        y_new = np.zeros_like(y)
+        for i in range(len(markers)):
+            y_new[y == markers_order[i]] = i
+        y = y_new
 
         ax.scatter(x, y, s=s, c=c, cmap=cmap, vmax=max(c) * 1.3)
 
@@ -139,6 +215,8 @@ def marker_plot(data, clusters, markers, gene_names=None,
             ax.axhline(h, c='k', linewidth=0.1, zorder=0)
         for v in np.unique(x):
             ax.axvline(v, c='k', linewidth=0.1, zorder=0)
+
+        ax.set_ylim(-0.5, len(markers) - 0.5)
 
         # Title
         title_fontsize = parse_fontsize(None, 'xx-large')
@@ -153,16 +231,15 @@ def marker_plot(data, clusters, markers, gene_names=None,
         shift_ticklabels(ax.xaxis, dx=0.1)
 
         # Y axis decorators
-        y_unique, y_unique_idx = np.unique(y, return_index=True)
-        label_axis(ax.yaxis, ticks=y_unique,
-                   ticklabels=np.array(marker_labels)[y_unique_idx])
+        label_axis(ax.yaxis, ticks=np.arange(len(markers)),
+                   ticklabels=markers[markers_order])
 
         if tissues is not None:
             # Right Y axis decorators
             ax2 = ax.twinx()
             ax2.set_ylim(ax.get_ylim())
-            label_axis(ax2.yaxis, ticks=y_unique,
-                       ticklabels=np.array(tissue_labels)[y_unique_idx])
+            label_axis(ax2.yaxis, ticks=np.arange(len(tissues)),
+                       ticklabels=tissues[markers_order])
 
         if show_fig:
             show(fig)
