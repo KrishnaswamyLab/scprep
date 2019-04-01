2112345a1b9ca7038188c72c8132a3edbfaa21fd
Scott Gigante <scottgigante@gmail.com>
2019-04-01 11:07:01 -0400

    change libpaths precedence?

--- travis_setup.R
+++ travis_setup.R
@@ -1,6 +1,6 @@
 lib.path <- "./.Rpackages"
 dir.create(lib.path)
-.libPaths(c(.libPaths(), lib.path))
+.libPaths(c(lib.path, .libPaths()))
 chooseCRANmirror(ind=1)
 install.packages("BiocManager", quietly=TRUE)
 BiocManager::install("splatter", quietly=TRUE)
