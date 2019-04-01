0346cd2ab6c4188c4673813af91285639f8da051
Scott Gigante <scottgigante@gmail.com>
2019-04-01 11:43:41 -0400

    cache R packages

--- travis_setup.R
+++ travis_setup.R
@@ -1,6 +1,3 @@
-lib.path <- "./.Rpackages"
-dir.create(lib.path)
-.libPaths(c(lib.path, .libPaths()))
 chooseCRANmirror(ind=1)
 install.packages("BiocManager", quietly=TRUE)
 BiocManager::install("splatter", quietly=TRUE)
