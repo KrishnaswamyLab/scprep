9e10d77937798fa9ec78b4535b12b1e30828ae26
Scott Gigante <scottgigante@gmail.com>
2019-04-01 11:53:56 -0400

    try again

--- travis_setup.R
+++ travis_setup.R
@@ -1,3 +1,6 @@
+lib.path <- "~/R/Library"
+dir.create(lib.path)
+.libPaths(c(lib.path, .libPaths()))
 chooseCRANmirror(ind=1)
 install.packages("BiocManager", quietly=TRUE)
 BiocManager::install("splatter", quietly=TRUE)
