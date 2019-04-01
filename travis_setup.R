2721676c8ae1349c0326c42b539b620ec88ebe3f
Scott Gigante <scottgigante@gmail.com>
2019-04-01 11:03:46 -0400

    install R packages to local

--- travis_setup.R
+++ travis_setup.R
@@ -1,3 +1,6 @@
+lib.path <- "./.Rpackages"
+dir.create(lib.path)
+.libPaths(c(.libPaths(), lib.path))
 chooseCRANmirror(ind=1)
 install.packages("BiocManager", quietly=TRUE)
 BiocManager::install("splatter", quietly=TRUE)
