2b799ff7debc2275fb0e1c977cf2bbad25130154
Scott Gigante <scottgigante@gmail.com>
2019-04-10 15:25:30 -0400

    handle output better

--- test/test_lazyload.py
+++ test/test_lazyload.py
@@ -4,7 +4,13 @@ from tools import data
 
 
 def test_lazyload():
-    return_code = subprocess.call(
+    proc = subprocess.Popen(
         ['nose2', '--quiet', '_test_lazyload'],
-        cwd=os.path.join(data._get_root_dir(), "test"))
-    assert return_code == 0
+        cwd=os.path.join(data._get_root_dir(), "test"), stderr=subprocess.PIPE)
+    return_code = proc.wait()
+    try:
+        assert return_code == 0
+    except AssertionError:
+        lines = proc.stderr.read().decode().split('\n')
+        lines = lines[4:-6]
+        raise AssertionError("\n".join(lines))
