c6d1ce133e4de8d42c5a8a16de3e2f9320845aec
Scott Gigante <scottgigante@gmail.com>
2019-04-10 14:26:54 -0400

    print which module failed

--- test/test_lazyload.py
+++ test/test_lazyload.py
@@ -6,4 +6,4 @@ def test_lazyload():
     loaded_modules = set([m.split('.')[0] for m in sys.modules.keys()])
     exceptions = ['matplotlib', 'mpl_toolkits']
     for module in scprep._lazyload._importspec.keys():
-        assert module not in loaded_modules.difference(exceptions)
+        assert module not in loaded_modules.difference(exceptions), module
