b32f6475671433d158de6a9f7b190ae5eca6b396
Scott Gigante <scottgigante@gmail.com>
2019-04-10 14:14:14 -0400

    test lazyloading

---
+++ test/test_lazyload.py
@@ -0,0 +1,9 @@
+import scprep
+import sys
+
+
+def test_lazyload():
+    loaded_modules = set([m.split('.')[0] for m in sys.modules.keys()])
+    exceptions = ['matplotlib', 'mpl_toolkits']
+    for module in scprep._lazyload.importspec.keys():
+        assert module not in loaded_modules.difference(exceptions)
