eb40a7fd0126d130d24e40bb81a187a395e3aa96
Scott Gigante <scottgigante@gmail.com>
2019-07-11 12:09:57 +0200

    document scprep.plot.colors.tab

--- scprep/plot/colors.py
+++ scprep/plot/colors.py
@@ -77,6 +77,28 @@ def tab40():
 
 
 def tab(n=10):
+    """A discrete colormap with an arbitrary number of colors
+
+    This colormap chooses the best of the following, in order:
+    - `plt.cm.tab10`
+    - `plt.cm.tab20`
+    - `scprep.plot.colors.tab30`
+    - `scprep.plot.colors.tab40`
+    - `scprep.plot.colors.tab10_continuous`
+
+    If the number of colors required is less than the number of colors
+    available, colors are selected specifically in order to reduce similarity
+    between selected colors.
+
+    Parameters
+    ----------
+    n : int, optional (default: 10)
+        Number of required colors.
+
+    Returns
+    -------
+    cmap : `matplotlib.colors.ListedColormap`
+    """
     if n < 1:
         raise ValueError(
             "Expected n >= 1. Got {}".format(n))
