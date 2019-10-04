3ba22b9c941a1da6ffb59ba8bc60d10a83a55eee
Scott Gigante <scottgigante@gmail.com>
2019-10-04 13:28:29 -0400

    patch pandas sparse dataframe loc, monkey patches https://github.com/pandas-dev/pandas/issues/27781

--- scprep/__init__.py
+++ scprep/__init__.py
@@ -14,3 +14,14 @@ import scprep.sanitize
 import scprep.stats
 import scprep.reduce
 import scprep.run
+
+import pandas as pd
+if int(pd.__version__.split('.')[1]) < 26:
+    def fill_value(self):
+        # Used in reindex_indexer
+        try:
+            return self.values.dtype.fill_value
+        except AttributeError:
+            return self.values.dtype.na_value
+    from pandas.core.internals.blocks import ExtensionBlock
+    setattr(ExtensionBlock, 'fill_value', property(fill_value))
--- test/test_patch.py
+++ test/test_patch.py
@@ -9,3 +9,7 @@ def test_pandas_series_rmatmul():
     df = pd.DataFrame(mat)
     ser = pd.Series(arr)
     np.testing.assert_array_equal(mat @ ser, (df @ ser).values)
+
+def test_pandas_sparse_iloc():
+    X = pd.DataFrame([[0,1,1], [0,0,1], [0,0,0]]).astype(pd.SparseDtype(float, fill_value=0.0))
+    assert np.all(~np.isnan(X.iloc[[0,1]].to_numpy()))
