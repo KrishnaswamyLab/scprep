e037c1a5e23a2a7efd0e774e2c75938c2a6f6f30
Scott Gigante <scottgigante@gmail.com>
2019-10-29 16:31:25 -0400

    make autoblack quiet

--- autoblack.sh
+++ autoblack.sh
@@ -6,8 +6,8 @@ set -e
 files=\$(git diff --staged --name-only --diff-filter=d -- "*.py")
 
 for file in \$files; do
-  black \$file
-    git add \$file
+  black -q \$file
+  git add \$file
 done
 EOF
 chmod +x .git/hooks/pre-commit
