5018b36de66f9c46f1a9d51ae2d6826cc3871835
Scott Gigante <scottgigante@gmail.com>
2020-10-28 17:14:20 -0400

    skip anndata2ri in py35

--- test/_test_lazyload.py
+++ test/_test_lazyload.py
@@ -12,6 +12,8 @@ def test_lazyload():
     postloaded_modules = set([m.split(".")[0] for m in sys.modules.keys()])
     scprep_loaded = postloaded_modules.difference(preloaded_modules)
     for module in scprep._lazyload._importspec.keys():
+        if module == "anndata2ri" and sys.version_info[:2] < (3, 6):
+            continue
         if module in preloaded_modules:
             assert getattr(scprep._lazyload, module).__class__ is type(scprep)
         else:
