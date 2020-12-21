e05d85db80c27fed512b8f00cebcdfecfef83d8e
Scott Gigante <scottgigante@users.noreply.github.com>
2020-12-21 11:40:58 -0500

    maxit 0 for loess

--- test/test_run.py
+++ test/test_run.py
@@ -314,6 +314,7 @@ else:
                 start_cluster=4,
                 omega=0.1,
                 smoother="loess",
+                maxit=0,
                 verbose=False,
             )
             pseudotime, branch, curves = (
