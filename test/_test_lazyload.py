ec69c140c502d5d3d3719f231dd23cf881ba6273
Scott Gigante <scottgigante@gmail.com>
2021-02-10 11:40:08 -0500

    more reporting

--- test/_test_lazyload.py
+++ test/_test_lazyload.py
@@ -15,10 +15,10 @@ def test_lazyload():
         if module == "anndata2ri" and sys.version_info[:2] < (3, 6):
             continue
         if module in preloaded_modules:
-            assert getattr(scprep._lazyload, module).__class__ is type(scprep)
+            assert getattr(scprep._lazyload, module).__class__ is type(scprep), module
         else:
             assert (
                 getattr(scprep._lazyload, module).__class__
                 is scprep._lazyload.AliasModule
-            )
+            ), module
         assert module not in scprep_loaded, module
