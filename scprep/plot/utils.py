986cdd60cea483e9a34c89b4cea0e1520a5330cf
Scott Gigante <scottgigante@gmail.com>
2021-02-18 10:34:12 -0500

    docs

--- scprep/plot/utils.py
+++ scprep/plot/utils.py
@@ -146,16 +146,21 @@ def parse_fontsize(size=None, default=None):
 
 
 class temp_fontsize(object):
+    """Context manager to temporarily change matplotlib font size."""
+
     def __init__(self, size=None):
+        """Initialize the context manager."""
         if size is None:
             size = plt.rcParams["font.size"]
         self.size = size
 
     def __enter__(self):
+        """Temporarily set the font size."""
         self.old_size = plt.rcParams["font.size"]
         plt.rcParams["font.size"] = self.size
 
     def __exit__(self, type, value, traceback):
+        """Change the font size back to default."""
         plt.rcParams["font.size"] = self.old_size
 
 
