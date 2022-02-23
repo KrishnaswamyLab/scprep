f7b416e862f1b9a931756713c9ec36b36ab7a89c
Scott Gigante <scott.gigante@immunai.com>
2022-02-23 13:43:33 -0500

    use a different example package

--- test/test_run.py
+++ test/test_run.py
@@ -44,18 +44,18 @@ else:
         )
 
     def test_install_github_lib():
-        scprep.run.install_github("twitter/AnomalyDetection", verbose=False)
+        scprep.run.install_github("mvuorre/exampleRPackage", verbose=False)
         fun = scprep.run.RFunction(
             body="""
             packages <- installed.packages()
-            'AnomalyDetection' %in% packages
+            'exampleRPackage' %in% packages
             """
         )
 
         assert fun()
 
     def test_install_github_dependencies_None():
-        scprep.run.install_github("twitter/AnomalyDetection", verbose=False)
+        scprep.run.install_github("mvuorre/exampleRPackage", verbose=False)
         fun = scprep.run.RFunction(
             body="""
             if (!require("pacman", quietly=TRUE)) {
@@ -73,7 +73,7 @@ else:
 
     def test_install_github_dependencies_True():
         scprep.run.install_github(
-            "twitter/AnomalyDetection", verbose=False, dependencies=True
+            "mvuorre/exampleRPackage", verbose=False, dependencies=True
         )
         fun = scprep.run.RFunction(
             body="""
